cd3f951e48c7e90d862eb2b16c2038509bbcf2f4
Phillip Cloud <cpcloud@gmail.com>
2015-03-02 10:19:31 -0500

    tests

--- req/test_req.py
+++ req/test_req.py
@@ -1,48 +1,49 @@
 from req import translate
 
+def f(a, b, c):
+    return a + b * c - 1
+
 
 def test_simple():
-    def f(a, b, c):
-        return a + b * c - 1
-
     q = translate(f)
-    assert q == 'f: {[a; b; c]}'
+    assert q == 'f: {[a; b; c] ((a) + ((b) * (c))) - (1)}'
+
+
+def g(a, b):
+    return a * b
 
 
 def test_binop():
-    def f(a, b):
-        return a * b
+    q = translate(g)
+    assert q == 'g: {[a; b] (a) * (b)}'
 
-    q = translate(f)
-    assert q == 'f: {[a; b] (a) * (b)}'
+
+def h(c, d):
+    x = c + 1
+    return x + 2
 
 
 def test_assign():
-    def h(c, d):
-        x = c + 1
-        return x + 2
-
     q = translate(h)
     assert q == 'h: {[c; d] x: (c) + (1); (x) + (2)}'
 
 
-def test_if():
-    def k(x):
-        if x == 1:
-            return x + 1
-        else:
-            return x * 2
+def k(x):
+    if x == 1:
+        return x + 1
+    else:
+        return x * 2
 
+
+def test_if():
     q = translate(k)
     assert q == 'k: {[x] $[(x) = (1); (x) + (1); (x) * (2)]}'
 
 
+def mapper(x):  # -> f each x
+    return map(f, x)
+
+
 def test_call():
-    def f(x):
-        return x + 1
-
-    def mapper(x):  # -> f each x
-        return map(f, x)
-
     q = translate(mapper)
     assert q == 'mapper: {[x] each[f; x]}'
